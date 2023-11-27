ebc7a2bcc566059f2528a1810e36d55d736231da
Hanshi <hanshitrash00@proton.me>
2023-11-27 23:30:56 +0800

    refactor(driver): apply best practice for driver code

--- main.py
+++ main.py
@@ -1,79 +1,71 @@
 from pso_1_var import PsoOneVar
 from pso_2_var import PsoTwoVar
-
 import random
 
-# PSO NOMOR 1
-# a.
-print("""
-      PSO NOMOR 1 a.
-""")
-x_1_var = [0, 1, 2]
-v_1_var = [0, 0, 0]
-r_1_var = [0.5, 0.5]
-c_1_var = [0.5, 1]
-w_1_var = 1
 
-pso_1_var = PsoOneVar(x_1_var, v_1_var, r_1_var, c_1_var, w_1_var)
-# pso_1_var.iter(3)
+def run_pso_1_var_a():
+    print("PSO NOMOR 1 a.")
+    x_1_var = [0, 1, 2]
+    v_1_var = [0, 0, 0]
+    r_1_var = [0.5, 0.5]
+    c_1_var = [0.5, 1]
+    w_1_var = 1
 
-# PSO NOMOR 1
-# b.
-print("""
-      PSO NOMOR 1 b.
-""")
+    pso_1_var = PsoOneVar(x_1_var, v_1_var, r_1_var, c_1_var, w_1_var)
+    # pso_1_var.iter(3)
 
-array_x0 = [random.randint(-5, 5) for i in range(9)]
 
-for i in range(len(array_x0)):
-    x0 = array_x0[i]
-    c = [0.5, 1]
-    w = 1
-    v = [0, 0, 0]
-    print("Perulangan ke-", i + 1, "dengan nilai x0 =", x0)
-    r1 = random.random()
-    r2 = random.random()
-    r = [r1, r2]
-    x = [x0, 1, 2]
-    pso = PsoOneVar(x, v, c, r, w)
-    # pso.plot_iteration(50)
+def run_pso_1_var_b():
+    print("PSO NOMOR 1 b.")
+    array_x0 = [random.randint(-5, 5) for i in range(10)]
 
-# PSO NOMOR 2
-# a.
+    for i, x0 in enumerate(array_x0):
+        c = [0.5, 1]
+        w = 1
+        v = [0, 0, 0]
 
-print("""
-      PSO NOMOR 2 a.
-""")
-x_2_var = [0, 1, 2]
-y_2_var = [0, 1, -1]
-v_2_var = [0, 0, 0]
-c_2_var = [1, 0.5]
-r_2_var = [1, 1]
-w_2_var = 1
+        r1, r2 = random.random(), random.random()
+        print(
+            f"Perulangan ke-{i + 1} dengan nilai x0 = {x0}, r1 = {r1}, r2 = {r2}")
+        r = [r1, r2]
+        x = [x0, 1, 2]
+        pso = PsoOneVar(x, v, c, r, w)
+        # pso.plot_iteration(50)
 
-pso_2_var = PsoTwoVar(x_2_var, y_2_var, v_2_var, c_2_var, r_2_var, w_2_var)
-# pso_2_var.iter(3)
 
-# PSO NOMOR 2
-# b.
+def run_pso_2_var_a():
+    print("PSO NOMOR 2 a.")
+    x_2_var = [0, 1, 2]
+    y_2_var = [0, 1, -1]
+    v_2_var = [0, 0, 0]
+    c_2_var = [1, 0.5]
+    r_2_var = [1, 1]
+    w_2_var = 1
 
-print("""
-      PSO NOMOR 2 b.
-""")
-array_x0_2_var = [random.randint(-10, 10) for i in range(9)]
-array_y0_2_var = [random.randint(-10, 10) for i in range(9)]
-# print(array_x0_2_var)
-# print(array_y0_2_var)
-for i in range(len(array_x0_2_var)):
-    x0 = array_x0_2_var[i]
-    y0 = array_y0_2_var[i]
-    c = [1, 0.5]
-    w = 1
-    v = [0, 0, 0]
-    r1 = random.random()
-    r2 = random.random()
-    r = [r1, r2]
-    x = [x0, 1, 2]
-    y = [y0, 1, -1]
-    pso_2_var_x_y = PsoTwoVar(x, y, v, c, r, w)
-    pso_2_var_x_y.plot_iteration(50)
+    pso_2_var = PsoTwoVar(x_2_var, y_2_var, v_2_var, c_2_var, r_2_var, w_2_var)
+    pso_2_var.iter(3)
+
+
+def run_pso_2_var_b():
+    print("PSO NOMOR 2 b.")
+    array_x0_2_var = [random.randint(-10, 10) for i in range(10)]
+    array_y0_2_var = [random.randint(-10, 10) for i in range(10)]
+
+    for i, (x0, y0) in enumerate(zip(array_x0_2_var, array_y0_2_var)):
+        c = [1, 0.5]
+        w = 1
+        v = [0, 0, 0]
+        r1, r2 = random.random(), random.random()
+        print(
+            f"Perulangan ke-{i + 1} dengan nilai x0 = {x0}, y0 = {y0}, r1 = {r1}, r2 = {r2}")
+        r = [r1, r2]
+        x = [x0, 1, 2]
+        y = [y0, 1, -1]
+        pso_2_var_x_y = PsoTwoVar(x, y, v, c, r, w)
+        pso_2_var_x_y.plot_iteration(50)
+
+
+# run_pso_1_var_a()
+# run_pso_1_var_b()
+# run_pso_2_var_a()
+run_pso_2_var_b()
--- tes.py
+++
@@ -1,4 +0,0 @@
-my_string = "Hello"
-
-for index, char in enumerate(my_string):
-    print(f"Index: {index}, Character: {char}")
